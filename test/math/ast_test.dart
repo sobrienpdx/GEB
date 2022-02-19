0421d39f9c3493521f20e95aa5ef6b711827b7b2
Paul Berry <stereotype441@gmail.com>
2022-02-18 19:27:21 -0800

    Clean up math tests

--- test/math/ast_test.dart
+++ test/math/ast_test.dart
@@ -1,41 +1,54 @@
 import 'package:geb/math/ast.dart';
+import 'package:geb/math/parse.dart';
 import 'package:test/test.dart';
 
 main() {
-  group('Pretty print', () {
-    test('propositional atom', () {
-      expect(P.toString(), 'P');
-      expect(Formula('P').toString(), 'P');
-      expect(PropositionalAtom('P′').toString(), 'P′');
-      expect(Formula('P′').toString(), 'P′');
-      expect(Formula("P'").toString(), 'P′');
-      expect(Formula('P′′').toString(), 'P′′');
-      expect(Formula("P''").toString(), 'P′′');
-    });
+  test('propositional atom', () {
+    expect(P.toString(), 'P');
+    expect(Formula('P').toString(), 'P');
+    expect(PropositionalAtom('P′').toString(), 'P′');
+    expect(Formula('P′').toString(), 'P′');
+    expect(Formula("P'").toString(), 'P′');
+    expect(Formula('P′′').toString(), 'P′′');
+    expect(Formula("P''").toString(), 'P′′');
+  });
 
-    test('not', () {
-      expect(Not(P).toString(), '~P');
-      expect(Formula('~P').toString(), '~P');
-      expect(Formula('~~P').toString(), '~~P');
-    });
+  test('not', () {
+    expect(Not(P).toString(), '~P');
+    expect(Formula('~P').toString(), '~P');
+    expect(Formula('~~P').toString(), '~~P');
+  });
 
-    test('and', () {
-      expect(And(P, Q).toString(), '<P∧Q>');
-      expect(Formula('<P∧Q>').toString(), '<P∧Q>');
-      expect(Formula('<P&Q>').toString(), '<P∧Q>');
-    });
+  test('and', () {
+    expect(And(P, Q).toString(), '<P∧Q>');
+    expect(Formula('<P∧Q>').toString(), '<P∧Q>');
+    expect(Formula('<P&Q>').toString(), '<P∧Q>');
+  });
 
-    test('or', () {
-      expect(Or(P, Q).toString(), '<P∨Q>');
-      expect(Formula('<P∨Q>').toString(), '<P∨Q>');
-      expect(Formula('<P|Q>').toString(), '<P∨Q>');
-    });
+  test('or', () {
+    expect(Or(P, Q).toString(), '<P∨Q>');
+    expect(Formula('<P∨Q>').toString(), '<P∨Q>');
+    expect(Formula('<P|Q>').toString(), '<P∨Q>');
+  });
 
-    test('implies', () {
-      expect(Implies(P, Q).toString(), '<P⊃Q>');
-      expect(Formula('<P⊃Q>').toString(), '<P⊃Q>');
-      expect(Formula('<P->Q>').toString(), '<P⊃Q>');
-    });
+  test('implies', () {
+    expect(Implies(P, Q).toString(), '<P⊃Q>');
+    expect(Formula('<P⊃Q>').toString(), '<P⊃Q>');
+    expect(Formula('<P->Q>').toString(), '<P⊃Q>');
+  });
+
+  test('complex parsing', () {
+    expect(Formula('<P∧Q>').toString(), '<P∧Q>');
+    expect(Formula('<P∧~P>').toString(), '<P∧~P>');
+    expect(Formula('<<P∨<Q⊃R>>∧<~P∨~R′>>').toString(), '<<P∨<Q⊃R>>∧<~P∨~R′>>');
+  });
+
+  test('parse errors', () {
+    expect(() => Formula('<P>'), throwsA(TypeMatcher<ParseError>()));
+    expect(() => Formula('<~P>'), throwsA(TypeMatcher<ParseError>()));
+    expect(() => Formula('<P∧Q∧R>'), throwsA(TypeMatcher<ParseError>()));
+    expect(() => Formula('<<P∧Q>∧<Q~∧P>>'), throwsA(TypeMatcher<ParseError>()));
+    expect(() => Formula('<P∧Q>∧<Q∧P>'), throwsA(TypeMatcher<ParseError>()));
   });
 }
 
